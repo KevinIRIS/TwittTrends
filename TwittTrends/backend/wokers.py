c2a95d78cc7f9336099b91c37236bb75208389eb
KevinIRIS <kz899@nyu.edu>
2017-04-13 20:23:13 -0400

    Sentiment function update

--- TwittTrends/backend/wokers.py
+++ TwittTrends/backend/wokers.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 from time import sleep
 
-from  watson_developer_cloud import natural_language_understanding_v1
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
 import watson_developer_cloud.natural_language_understanding.features.v1 as \
     features
 import boto3
@@ -17,14 +17,15 @@ def analyse(queue):
     messages = queue.receive_messages(MessageAttributeNames=['All'], VisibilityTimeout=30, MaxNumberOfMessages=10)
     for message in messages:
         if message.body is not None and message.message_attributes is not None:
-            print(message.body)
-            nlp = natural_language_understanding_v1(version='2017-02-27',\
-                username= configure.ibm_username,\
-                password= configure.ibm_password)
-            print(nlp)
-            response = nlp.analyze(text = message.body, features = [features.Entities(), features.Keywords()])
-            print("sss")
-            print(response)
+            #print(message.body)
+            nlp = NaturalLanguageUnderstandingV1(version='2017-02-27',\
+                url = "https://gateway.watsonplatform.net/natural-language-understanding/api",\
+                username = configure.ibm_username,\
+                password = configure.ibm_password)
+            response = nlp.analyze(text = message.body, features=[features.Sentiment()])
+            print(response["sentiment"]["document"]["label"])
+            boto3.resource("sns", aws_access_key_id = configure.aws_access_key_id, \
+                           aws_secret_access_key = configure.aws_secret_access_key)
 
 
 
